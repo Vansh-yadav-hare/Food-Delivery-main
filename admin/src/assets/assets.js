e0422d8a7ef58a371e5493fd4e14a061a28001ee
Vansh Yadav <vy111106@gmail.com>
2026-01-23 16:09:24 +0530

    Update API URL for food delivery backend

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-s9we.onrender.com'
+export const url = 'https://food-delivery-backend-qnsm.onrender.com'
